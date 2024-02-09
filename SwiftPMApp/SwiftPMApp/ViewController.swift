1a415305cd5e9b833a6e284c20140f4f51ea73a2
HarumaIto <itoharm@gmail.com>
2024-02-09 22:32:08 +0900

    add SwiftPMApp

---
+++ SwiftPMApp/SwiftPMApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftPMApp
+//
+//  Created by Haruma Ito on 2024/02/09.
+//
+
+import UIKit
+
+import PKHUD
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        HUD.flash(.success, delay: 2.0)
+    }
+
+
+}
+
